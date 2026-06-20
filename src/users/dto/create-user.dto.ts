725d49ede85cf41908486295fbaf122ecb99a6e0
Lucas Previato <lucaspreviato@Live.com>
2026-06-20 19:15:19 -0300

    feat(users): adiciona validação no CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+import { IsEmail, IsString } from "class-validator";
+
+export class CreateUserDto {
+    @IsString({ message: 'name must be a string' })
+    name!: string;
+    @IsEmail({}, { message: 'email must be a valid email' })
+    email!: string;
+    @IsString({ message: 'password must be a string' })
+    password!: string;
+}
