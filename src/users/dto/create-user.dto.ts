d0d5a8f325520ab2b429225dbc6f256dc41fecc7
Lucas Previato <lucaspreviato@Live.com>
2026-06-20 19:34:33 -0300

    feat(users): corrigindo a exportaçao via cjs no prisma, habilitando validação na main, melhorando validações no dtos e habilitando .http para facilitar chamadas

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,13 @@
-import { IsEmail, IsString } from "class-validator";
+import { IsEmail, IsString, MinLength } from "class-validator";
 
 export class CreateUserDto {
+    
     @IsString({ message: 'name must be a string' })
+    @MinLength(3, { message: 'name must be at least 3 characters long' })
     name!: string;
     @IsEmail({}, { message: 'email must be a valid email' })
     email!: string;
     @IsString({ message: 'password must be a string' })
+    @MinLength(8, { message: 'password must be at least 8 characters long' })
     password!: string;
 }
