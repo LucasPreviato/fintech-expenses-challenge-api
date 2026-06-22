a67e260ce82fe3aea57a21925d093e6d12c377f6
Lucas Previato <lucaspreviato@Live.com>
2026-06-21 21:57:38 -0300

    feat(users): criacao da camada de services do module user
    - hash password para protecao das senhas dos usuarios
    - listagem de users paginada com helpers compartilhado
    - entity para melhorar a tipagem e semantica dos metodos do module

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class User {}
+export class UserEntity {
+  id!: string;
+  name!: string;
+  email!: string;
+  createdAt!: Date;
+  updatedAt!: Date;
+  deletedAt!: Date | null;
+
+  constructor(partial: Partial<UserEntity>) {
+    Object.assign(this, partial);
+  }
+}
