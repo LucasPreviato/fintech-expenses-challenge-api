b34759965b0980876e2bf158732cc160933262c1
Lucas <lucaspreviato@live.com>
2026-06-22 09:45:08 -0300

    fix: ajustes minimos para deploy no render e controle de healthz no painel

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { Public } from './auth/decorators/public.decorator';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Public()
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('healthz')
+  getHealthz() {
+    return { status: HttpStatus.OK };
   }
 }
